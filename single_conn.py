e91e9fa9969f4db6308c2bf1fb81722a39a00df7
MarcusJP <javin.marc@gmail.com>
2019-04-29 17:03:42 +0200

    beautiful gui

--- single_conn.py
+++ single_conn.py
@@ -3,14 +3,10 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.patches import ArrowStyle
-from ipywidgets import interact, IntSlider
 
-def init():
-    dfs = pd.read_csv('data/Neuro279_Syn.csv', index_col=0)
-    dfg = pd.read_csv('data/Neuro279_EJ.csv', index_col=0)
-    return dfs, dfg
+dfs = pd.read_csv('data/Neuro279_Syn.csv', index_col=0)
+dfg = pd.read_csv('data/Neuro279_EJ.csv', index_col=0)
     
-dfs, dfg = init()
 dfcat = pd.read_csv('data/neuron_categories.csv', index_col=1, header=0)
 nbs = [i for i in range(len(dfs.index))]
 cats = dfcat.loc[dfg.index]
@@ -39,16 +35,16 @@ def connections(neuron='BAGL'):
     intens_in = dfs.loc[dfs[neuron] > 0, neuron]
     syni = [(pre, neuron, {}) for pre in syn_in]
     G.add_edges_from(syni)
+
+    gaps_ = dfg.index[dfg[neuron] > 0].tolist()
+    intens_g = 0.1*dfg.loc[dfg[neuron] > 0, neuron]
+    gaps = [(neuron, k, {}) for k in gaps_] + [(k, neuron, {}) for k in gaps_]
+    G.add_edges_from(gaps)
     
     syn_out = dfs.T.index[dfs.T[neuron] > 0].tolist()
     intens_out = dfs.T.loc[dfs.T[neuron] > 0, neuron]
     syno = [(neuron, post, {}) for post in syn_out]
     G.add_edges_from(syno)
-    
-    gaps_ = dfg.index[dfg[neuron] > 0].tolist()
-    intens_g = 0.1*dfg.loc[dfg[neuron] > 0, neuron]
-    gaps = [(neuron, k, {}) for k in gaps_] + [(k, neuron, {}) for k in gaps_]
-    G.add_edges_from(gaps)
     
     G.remove_node(neuron)
     pos = nx.layout.circular_layout(G, scale=2)
@@ -67,43 +63,76 @@ def connections(neuron='BAGL'):
     
     nx.draw_networkx_edges(G, pos, arrowstyle=style, edgelist=syni, edge_color='g',
                            arrowsize=10, alpha=0.7, width=intens_in, node_size=NODE_SIZE)
-    nx.draw_networkx_edges(G, pos, arrowstyle=style, edgelist=syno, edge_color='r',
-                           arrowsize=10, alpha=0.5, width=intens_out, node_size=NODE_SIZE)
     nx.draw_networkx_edges(G, pos, arrowstyle=styleg, edgelist=gaps, edge_color='Gold',
                            arrowsize=10, alpha=0.8, width=np.hstack((intens_g,intens_g)), 
                            node_size=NODE_SIZE)
+    nx.draw_networkx_edges(G, pos, arrowstyle=style, edgelist=syno, edge_color='r',
+                           arrowsize=10, alpha=0.5, width=intens_out, node_size=NODE_SIZE)
     plt.axis('off')
 
 
-from tkinter import *
-from tkinter.ttk import Combobox 
-from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
 
-class App:
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 
-    def __init__(self, master, img_size=6):
-        fig = plt.figure(figsize=(img_size,img_size))
-        self.canvas = FigureCanvasTkAgg(fig, master=master)  # A tk.DrawingArea.
-        self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
-        
-        options = sorted(dfs.index)
-        self.neur = StringVar(master)
-        self.neur.set("BAGL")
+class Visu(QVBoxLayout):
 
-        w = Combobox(master, textvariable=self.neur, values=options)
-        w.bind("<<ComboboxSelected>>", self.draw)
-        w.pack()
+    nb = 0
+
+    def __init__(self, img_size=12):
+        super(Visu, self).__init__()
+
+        Visu.nb += 1
+        self.nb = Visu.nb
+        self.figure = plt.figure(self.nb, figsize=(img_size,img_size))
+        self.canvas = FigureCanvas(self.figure)
+
+        subl = QHBoxLayout()
+        self.cb = QComboBox()
+        self.cb.addItems(sorted(dfs.index))
+        self.cb.currentIndexChanged.connect(self.draw)
+        self.cb.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        self.addWidget(self.canvas)
+        subl.addStretch()
+        subl.addWidget(self.cb)
+        subl.addStretch()
+        self.addLayout(subl)
 
     def draw(self, event=None):
-        plt.clf()
-        connections(self.neur.get())
+        self.figure.clf()
+        plt.figure(self.nb)
+        connections(self.cb.currentText())
         self.canvas.draw()
 
-        
-        
+class Window(QWidget):
 
-root = Tk()
-app = App(root)
-root.mainloop()
+    def __init__(self):
+        super(Window, self).__init__()
+
+        layout = QVBoxLayout()
+        self.visu_layout = QHBoxLayout()
+
+        self.b1 = QPushButton("Add neuron")
+        self.b1.clicked.connect(self.add_visu)
+
+        layout.addWidget(self.b1)
+        self.visu_layout.addLayout(Visu())
+        layout.addLayout(self.visu_layout)
+        self.setLayout(layout)
+
+        self.resize(500, 500)
+        self.move(300, 300)
+        self.setWindowTitle("Connectome Visualizer")
+
+    def add_visu(self):
+        self.visu_layout.addLayout(Visu())
+
+
+app = QApplication(sys.argv)
+w = Window()
+w.show()
+sys.exit(app.exec_())
 
